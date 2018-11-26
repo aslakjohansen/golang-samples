42038c7bc0f7323cfada166794cc6750867d3fd5
Aslak Johansen <aslakj@gmail.com>
2018-11-26 22:51:43 +0100

    Added an example of a cast

--- practut/types/types.go
+++ practut/types/types.go
@@ -9,7 +9,9 @@ func add (x float64, y float64) float64 {
 func main () {
     const num1 float64 = 5.6
     var   num2 float64 = 9.5
+    var   a    int     = 12
+    var   b    float64 = float64(a)
     
-    fmt.Println(add(num1, num2))
+    fmt.Println(add(num1, num2)+b)
 }
 
