2d5571cd3806ecd707a61312bfd8b54967730171
Aslak Johansen <aslakj@gmail.com>
2018-11-27 16:22:28 +0100

    Now with two handlers
    Note: The index handler is also -- for some reason -- the default fallback handler

--- practut/primitive_web/primitive_web.go
+++ practut/primitive_web/primitive_web.go
@@ -9,8 +9,13 @@ func index_handler (w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "You have reached the root!")
 }
 
+func about_handler (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "About what?")
+}
+
 func main () {
     http.HandleFunc("/", index_handler)
+    http.HandleFunc("/about", about_handler)
     http.ListenAndServe(":8000", nil)
 }
 
