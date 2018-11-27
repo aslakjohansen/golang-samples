cfc7c3d8383d49d331e101f5d3fc5966ac4371b6
Aslak Johansen <aslakj@gmail.com>
2018-11-27 16:19:56 +0100

    First iteration of the primitive web example
    https://www.youtube.com/watch?v=-QmdZ7821wA

---
+++ practut/primitive_web/primitive_web.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func index_handler (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "You have reached the root!")
+}
+
+func main () {
+    http.HandleFunc("/", index_handler)
+    http.ListenAndServe(":8000", nil)
+}
+
