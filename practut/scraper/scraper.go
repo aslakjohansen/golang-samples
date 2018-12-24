9fea9ae5abc9920f5127660251a4466b4c91b788
Aslak Johansen <aslakj@gmail.com>
2018-12-24 23:40:38 +0100

    Fixed The 404 error

--- practut/scraper/scraper.go
+++ practut/scraper/scraper.go
@@ -7,75 +7,77 @@ import (
     "encoding/xml"
 )
 
-//<?xml version="1.0" encoding="UTF-8"?>
-//<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-opinions-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-local-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-local-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-sports-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-sports-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-national-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-national-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-world-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-world-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-business-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-business-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-technology-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-technology-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-lifestyle-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-lifestyle-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-entertainment-sitemap.xml</loc>
-//   </sitemap>
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-entertainment-sitemap.xml</loc>
-//   </sitemap>  
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-blogs-goingoutguide-sitemap.xml</loc>
-//   </sitemap>   
-//   <sitemap>
-//      <loc>http://www.washingtonpost.com/news-goingoutguide-sitemap.xml</loc>
-//   </sitemap> 
-//</sitemapindex>
+var washPostXML = []byte(`
+<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-opinions-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-local-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-local-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-sports-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-sports-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-national-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-national-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-world-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-world-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-business-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-business-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-technology-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-technology-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-lifestyle-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-lifestyle-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-entertainment-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-entertainment-sitemap.xml</loc>
+   </sitemap>  
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-goingoutguide-sitemap.xml</loc>
+   </sitemap>   
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-goingoutguide-sitemap.xml</loc>
+   </sitemap> 
+</sitemapindex>
+`)
 
 type SitemapIndex struct {
     Locations []string `xml:"sitemap>loc"`
@@ -91,9 +93,10 @@ func main () {
     var s SitemapIndex
     var n News
     
-    resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-    bytes, _ := ioutil.ReadAll(resp.Body)
-    resp.Body.Close()
+//    resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+//    bytes, _ := ioutil.ReadAll(resp.Body)
+//    resp.Body.Close()
+    bytes := washPostXML
     fmt.Println(string(bytes))
     xml.Unmarshal(bytes, &s)
     fmt.Println(s)
