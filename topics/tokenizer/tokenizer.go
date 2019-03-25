f58033a46e55b9a642d9c365db990d8da4eab40e
Aslak Johansen <aslakj@gmail.com>
2019-03-25 13:55:12 -0700

    Added a few structs for ... structure

--- topics/tokenizer/tokenizer.go
+++ topics/tokenizer/tokenizer.go
@@ -14,6 +14,21 @@ const (
     LPAR
     RPAR
 )
+type token_constructor func(c chan int, begin_line int, begin_col int, end_line int, end_col int)
+
+type state struct {
+    constructor token_constructor
+}
+
+type tokenizer struct {
+    states []state
+    fill int
+}
+
+func (t *tokenizer) init () {
+    t.states = make([]state, 0, 0)
+    t.fill = 0
+}
 
 type token struct {
     class      TokenClass
@@ -23,7 +38,7 @@ type token struct {
     end_col    int
 }
 
-func tokenize (input string) {
+func (t *tokenizer) tokenize (input string) {
 //    var start int = 0
     var begin_line  int = 1
     var begin_col   int = 1
@@ -33,6 +48,12 @@ func tokenize (input string) {
     for i := range input {
         fmt.Printf("input[%d] = '%d'\n", i, input[i])
         
+        // temp fix to keep golang happy
+        if true {
+            begin_line = begin_col
+            begin_col = begin_line
+        }
+        
         begin_line = end_line
         begin_col  = end_col
     }
@@ -41,8 +62,11 @@ func tokenize (input string) {
 func main () {
     fmt.Println("Tokenizer")
     
+    var t tokenizer = tokenizer{}
+    t.init()
+    
     var input string = "1.2+ (13-\npi/2)*e ^2";
     
     fmt.Println("input: "+input)
-    tokenize(input)
+    t.tokenize(input)
 }
