db207b7e4cf7d08a372132bb7d05eed43ebc1c84
Aslak Johansen <aslakj@gmail.com>
2019-10-18 09:30:30 +0200

    With parser
    Note: I get a syntax error. Because the root is not defined?

--- topics/lexmachine/test.go
+++ topics/lexmachine/test.go
@@ -2,67 +2,25 @@ package main
 
 import (
     "fmt"
-    
-    "github.com/timtadh/lexmachine"
-    "github.com/timtadh/lexmachine/machines"
+    "os"
     
     "golang-samples/topics/lexmachine/lang"
 )
 
-//var NUMBER int = 1
-//var PLUS   int = 2
-var SPACE  int = 3
-
-func token (id int) lexmachine.Action {
-    return func (s *lexmachine.Scanner, m *machines.Match) (interface{}, error) {
-        return s.Token(id, string(m.Bytes), m), nil
-    }
-}
-
-func newLexer (dfa bool) *lexmachine.Lexer {
-    var lexer = lexmachine.NewLexer()
-    lexer.Add([]byte(`\+`), token(lang.PLUS))
-    lexer.Add([]byte(`[0-9]*\.?[0-9]+`), token(lang.NUMBER))
-    lexer.Add([]byte(`\s+`), token(SPACE))
-    
-    var err error
-    if dfa {
-        err = lexer.CompileDFA()
-    } else {
-        err = lexer.CompileNFA()
-    }
-    if err != nil {
-        panic(err)
-    }
-    return lexer
-}
-
-func scan (lexer *lexmachine.Lexer, input string) error {
-    scanner, err := lexer.Scanner([]byte(input))
-    if err != nil {
-        panic(err)
-    }
-    
-    for tok, err, eos := scanner.Next(); !eos; tok, err, eos = scanner.Next() {
-        if ui, is := err.(*machines.UnconsumedInput); is {
-            // skip the error via:
-            // scanner.TC = ui.FailTC
-            //
-            fmt.Println("scan: ui=", ui)
-            fmt.Println("scan: is=", is)
-            return err
-        } else if err != nil {
-            return err
-        }
-        fmt.Println(tok)
-    }
-    return nil
-}
-
 func main () {
-    var lexer = newLexer(true)
+    var lexer = lang.NewLexer(true)
     
-    var input string = "3.14 + 7"
-    var err = scan(lexer, input)
-    fmt.Println("main: error=", err)
+    var input string = "3.14+7"
+    
+//    var err = scan(lexer, input)
+//    fmt.Println("main: error=", err)
+    
+    stmts, err := lang.Parse(lexer, input)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+    for _, stmt := range stmts {
+        fmt.Println(stmt)
+    }
 }
