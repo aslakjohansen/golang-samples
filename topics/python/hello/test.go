ed5283e95aaa8aa2ea8a09f9e9aad172a4f27bc1
Aslak Johansen <aslakj@gmail.com>
2020-06-28 11:34:15 +0200

    Added an embedded python hello world

---
+++ topics/python/hello/test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    python "github.com/sbinet/go-python"
+)
+
+func main () {
+    err := python.Initialize()
+    if err != nil {
+        fmt.Println("Unable to initialize python", err)
+    }
+    
+    module := python.PyImport_ImportModule("module")
+    if module == nil {
+        fmt.Println("Unable to import module")
+    }
+    
+    hello := module.GetAttrString("hello")
+    if module == nil {
+        fmt.Println("Unable to name function in module")
+    }
+    
+    state := python.PyEval_SaveThread()
+    gstate := python.PyGILState_Ensure()
+    
+    hello.Call(python.PyTuple_New(0), python.PyDict_New())
+    
+    python.PyGILState_Release(gstate)
+    python.PyEval_RestoreThread(state)
+    
+    err = python.Finalize()
+    if err != nil {
+        fmt.Println("Unable to finalize python", err)
+    }
+}
+
